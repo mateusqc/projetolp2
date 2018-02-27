316c9f54bd85cfe6b6bc8d47387093584b5c0710
LucasCBrasil <lucas.brasil@ccc.ufcg.edu.br>
2018-02-27 18:50:37 -0300

    Atualização dos teste de Aluno. Verifiquem se ainda faltam testes serem realizados e que não implementei

--- tests/projeto/AlunoTest.java
+++ tests/projeto/AlunoTest.java
@@ -10,33 +10,75 @@ import org.junit.rules.ExpectedException;
 public class AlunoTest {
 
 	private Aluno aluno, alunoDois;
-	private AlunoController controller;
 
 	
 	@Before
-
 	public void setUp(){
 		aluno = new Aluno("vitor", "00000", 12345, "3321-5489", "vitor@ccc.ufcg.edu.br");
 		
 	}
 	
 	@Test
-	public void testConstrutorComDadosValidos() {
-		Assert.assertEquals("vitor", aluno.getNome());
-		Assert.assertEquals("00000", aluno.getMatricula());
-		Assert.assertEquals(12345, aluno.getCodigoCurso());
-		Assert.assertEquals("3321-5489", aluno.getTelefone());
-		Assert.assertEquals("vitor@ccc.ufcg.edu.br", aluno.getEmail());
-		
+	public void testCriaAlunoPassandoTelefone() {
+		aluno = new Aluno("Lucas", "1000", 4000, "3335-1204", "lucas.brasil@ccc.ufcg.edu.br");
+		assertEquals(aluno.toString(), "1000 - Lucas - 4000 - 3335-1204 - lucas.brasil@ccc.ufcg.edu.br");
 	}
-	@Test
 	
+	@Test
 	public void testToString(){
 		Assert.assertEquals("00000 - vitor - 12345 - 3321-5489 - vitor@ccc.ufcg.edu.br", aluno.toString());
 	}
 	
-
+	@Test
+	public void testCriaAlunoSemPassarTelefone() {
+		
+		aluno = new Aluno("Lucas", "1000", 4000,"", "lucas.brasil@ccc.ufcg.edu.br");
+		assertEquals(aluno.toString(), "1000 - Lucas - 4000 - lucas.brasil@ccc.ufcg.edu.br");
+		
 	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCriaAlunoNomeVazio() {
+		
+		aluno = new Aluno("  ", "1000", 4000, "", "lucas.brasil@ccc.ufcg.edu.br");
+		
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testCriaAlunoNomeNull() {
+		
+		aluno = new Aluno(null, "1000", 4000, "", "lucas.brasil@ccc.ufcg.edu.br");
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCriaAlunoComEmailSemArroba() {
+		
+		aluno = new Aluno("Lucas", "1000", 4000, "", "lucas.brasil.ccc.ufcg.edu.br");
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCriaAlunoComEmailInvalidoArrobaNoComeco() {
+		
+		aluno = new Aluno("Lucas", "1000", 4000, "", "@lucas.brasil.ccc.ufcg.edu.br");
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCriaAlunoComEmailInvalidoArrobaNoFinal() {
+		
+		aluno = new Aluno("Lucas", "1000", 4000, "", "lucas.brasil.ccc.ufcg.edu.br@");
+		
+	}
+	
+	@Test
+	public void testCompareToDeDoisAlunos() {
+		
+		alunoDois = new Aluno("Lucas", "1000", 4000, "", "lucas.brasil@ccc.ufcg.edu.br");
+		assert(aluno.compareTo(alunoDois) > 0);
+		
+	}
+}
 	
 
