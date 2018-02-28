b2be82d6dfdfb3e01bcd06419cb2ee3b7092c9c1
mateusqc <mateusqcunha@gmail.com>
2018-02-27 21:54:50 -0300

    Documentação de TutorController

--- src/projeto/TutorController.java
+++ src/projeto/TutorController.java
@@ -19,10 +19,20 @@ import java.util.Map;
 public class TutorController {
 	private Map<String, Tutor> tutores;
 	
+	/**
+	 * Construtor de TutorController. Inicializa o mapa de tutores.
+	 */
 	public TutorController() {
 		this.tutores = new HashMap<String, Tutor>();
 	}
 	
+	/**
+	 * Método que torna um aluno tutor. O Tutor irá armazenar o Aluno que virou tutor como parâmetro, por isso é passado o objeto aluno.
+	 * @param matricula matrícula do aluno
+	 * @param disciplina primeira disciplina da tutoria
+	 * @param proficiencia proeficiência na primeira disciplina 
+	 * @param aluno objeto de referência ao aluno que virou tutor
+	 */
 	public void tornarTutor(String matricula, String disciplina, int proficiencia, Aluno aluno) {
 		if(proficiencia < 1 || proficiencia > 5) {
 			throw new IllegalArgumentException("Erro na definicao de papel: Proficiencia invalida");
@@ -33,7 +43,12 @@ public class TutorController {
 			this.tutores.get(matricula).adicionarDisciplina(disciplina, proficiencia);
 		}
 	}
-
+	
+	/**
+	 * Método que obtém as informações do tutor com a respectiva matrícula informada e as retorna numa String.
+	 * @param matricula matrícula do tutor
+	 * @return String com as informações do Tutor
+	 */
 	public String recuperaTutor(String matricula) {
 		if(!tutores.containsKey(matricula)) {
 			throw new IllegalArgumentException("Erro na busca por tutor: Tutor nao encontrado");
@@ -43,6 +58,10 @@ public class TutorController {
 	
 	}
 
+	/**
+	 * Método que gera uma listagem dos tutores cadastrados no sistema, ordenada em ordem alfabética com base no nome do tutor.
+	 * @return String com a listagem ordenada dos tutores
+	 */
 	public String listarTutores() {
 		
 		List<Tutor> tutoresOrdenadosNome = new ArrayList<Tutor>();
